cedb80e0b49b275a7f134fd9892a6b8041ae7842
“dowilliams” <dowilliams@gmail.com>
2022-04-27 11:07:24 -0500

    react.js styling lesson updates

---
+++ ReactApps/react-styling/src/styles/Styles.js
@@ -0,0 +1,8 @@
+import `./Styles.css`
+
+function Styles() {
+    return (
+        // To style a React Component, a class name can be added to the top level/parent element. This allows for styles to be applied to a specific component.
+    )
+}
+
